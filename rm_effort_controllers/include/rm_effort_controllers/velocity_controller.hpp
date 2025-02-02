1479dffa3300f864de489a7dee945367a60c9c2d
1moule <1961466188@qq.com>
2025-02-02 20:41:05 +0800

    Add rm effort controllers.

---
+++ rm_effort_controllers/include/rm_effort_controllers/velocity_controller.hpp
@@ -0,0 +1,8 @@
+//
+// Created by guanlin on 25-2-2.
+//
+
+#ifndef VELOCITY_CONTROLLER_HPP
+#define VELOCITY_CONTROLLER_HPP
+
+#endif //VELOCITY_CONTROLLER_HPP
---
+++ rm_effort_controllers/src/velocity_controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by guanlin on 25-2-2.
+//
+
+#include "rm_effort_controllers/velocity_controller.hpp"
